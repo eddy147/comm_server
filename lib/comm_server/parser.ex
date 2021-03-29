e6406c2a1f5626c53fe72548faf8e8a3d4aa5b6c
Eddy <eddy147@gmail.com>
2021-03-29 12:40:50 +0200

    Get the BSN

--- lib/comm_server/parser.ex
+++ lib/comm_server/parser.ex
@@ -1,7 +1,18 @@
 defmodule CommServer.Parser do
   alias CommServer.Message
 
+  def get_sender_agb(%Message{type: "JW315", xml: xml}) do
+    l = xml |> Quinn.XmlParser.parse()
+    List.flatten(l)
+    xml |> Quinn.XmlParser.parse() |> Quinn.find(String.to_atom("jw315:Afzender"))
+  end
+
   def get_client_bsn(%Message{type: "JW315", xml: xml}) do
-    xml |> Quinn.XmlParser.parse() |> Quinn.find(String.to_atom("jw315:Bsn"))
+    xml
+    |> Quinn.XmlParser.parse()
+    |> Quinn.find(String.to_atom("jw315:Bsn"))
+    |> List.first()
+    |> Map.fetch!(:value)
+    |> List.first()
   end
 end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -8,12 +8,9 @@ defmodule ParserTest do
 
   @fixtures_path Path.expand("./fixtures", __DIR__)
 
-  test "Get Client Bsn from Jw315" do
+  test "Get Sender from Jw315" do
     bsn = Parser.get_client_bsn(%Message{type: "JW315", xml: read_xml()})
-    |> List.first()
-    IO.inspect(bsn)
-    [bsn_value] = bsn.value
-    assert "999900006" == bsn_value
+    assert "999900006" == bsn
   end
 
   defp read_xml() do
